6109eb4841d85b203a9f7f92a89f805d8f52960b
CharanMann <charan.mann@forgerock.com>
2016-05-16 21:43:41 -0400

    Updated hostname

--- TravelApp/config/env/development.js
+++ TravelApp/config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: 8012,
 	applicationContext: '/travelApp',
-	url: 'http://travel.sc-int.com:8012/travelApp/',
-	OpenIG_url: 'http://travel-ig.sc-int.com:9002/travelApp/'
+	url: 'http://travel.sample.com:8012/travelApp/',
+	OpenIG_url: 'http://travel-ig.sample.com:9002/travelApp/'
 };
