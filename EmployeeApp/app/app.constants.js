4138bb606305178ba6ceb5c9da8e137da59b3904
CharanMann <charan.mann@forgerock.com>
2016-05-20 11:02:56 -0400

    Added travel portal link

--- EmployeeApp/app/app.constants.js
+++ EmployeeApp/app/app.constants.js
@@ -4,6 +4,8 @@ angular.module('app.constants', [])
         openAMURL: 'http://openam13.sc.com:8080/openam',
         apisURL: 'http://apis.sc.com:8010/history/',
         apisURLOpenIG: 'http://apis-ig.sc.com:9002/history/',
+        travelAppURL: 'http://travel.sample.com:8012/travelApp/',
+        travelAppURLOpenIG: 'http://travel-ig.sample.com:9002/travelApp/',
         clientID: 'employeeApp',
         redirectURI: 'http://employees-ig.sc.com:9000/employeeApp/oauth2_callback.html'
      });
